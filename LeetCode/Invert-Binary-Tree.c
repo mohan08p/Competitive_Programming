5c0c8114595de92c28fcb00896ac6a610892bdff
Pranav Ganorkar <pranavg189@gmail.com>
2016-02-06 23:06:58 +0530

    added problem statements to leetcode problems

--- LeetCode/Invert-Binary-Tree.c
+++ LeetCode/Invert-Binary-Tree.c
@@ -1,3 +1,22 @@
+/* Problem Statement:
+
+Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+*/
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
--- LeetCode/Validate-Binary-Search-Tree-98.cpp
+++ LeetCode/Validate-Binary-Search-Tree-98.cpp
@@ -1,4 +1,16 @@
-/* The important point to realize in this problem is that
+/* Problem Statement:
+
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+1)The left subtree of a node contains only nodes with keys less than the node's key.
+2)The right subtree of a node contains only nodes with keys greater than the node's key.
+3)Both the left and right subtrees must also be binary search trees.
+
+*/
+
+/* Hint: The important point to realize in this problem is that
 if we store the inorder traversal of the binary search tree 
 it should be sorted in ascending order */
 
