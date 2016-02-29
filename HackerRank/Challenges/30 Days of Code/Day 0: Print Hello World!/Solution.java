83daccc24e00290e37faf6abb7715a5986dfb676
mohan08p <mohanpawary1990@gmail.com>
2016-02-29 05:34:37 +0000

    30 Days of Code

---
+++ HackerRank/Challenges/30 Days of Code/Day 0: Print Hello World!/Solution.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class HelloWorld {
+
+       public static void main(String[] args) {
+        
+          // Write your print statements here!! 
+          // Template: System.out.println("what you want to print");
+           System.out.println("Hello World.");
+           System.out.println("Welcome to 30 Days of Code.");
+       }
+   }
+
+
